e0bd058be8810a650a9a3737da1f9db7086cd799
shashank <tiwarishashank1229@gmail.com>
2024-10-18 21:33:46 +0530

    Initial commit with add_num and input processing functions

--- test.py
+++ test.py
@@ -1,1 +1,22 @@
-print('hello')
+import re
+
+numbers = re.compile('-?\d+')
+
+def add_num(array):
+    neg_array = []
+    for num in array:
+        if num < 0:
+            neg_array.append(num)
+    if neg_array:
+        return f'negative numbers not allowed {", ".join(map(str, neg_array))}'
+    else:
+        x = sum(array)
+        return f'Sum is {x}'
+
+def process_input(inp):
+    if not inp:
+        return 'Input cannot be empty'
+    
+    result = list(map(int, numbers.findall(inp)))
+    return add_num(result)
+
